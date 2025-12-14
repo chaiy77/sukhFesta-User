1d063b71f5471ca50e28b4108822bcaae6baf254
chaiy <chaiy77@gmail.com>
2025-12-14 18:31:15 +0700

    create qrcode from token

--- src/components/user/myQR.js
+++ src/components/user/myQR.js
@@ -1,17 +1,18 @@
 "use client";
 
 import QrCodeGenerator from "../qrcode/qrGenerator";
+import { useUserContext } from "@/context/userContext";
 
-const MyQRComponent = ({ gotoPage, qrCodeData }) => {
+const MyQRComponent = ({ gotoPage }) => {
   // ShopID For Dev
   // We need to send request to dynamobd via id token
-  qrCodeData = "587868ee-e067-4e4b-a982-a6cc9ed60b17";
+  const { lineToken } = useUserContext();
 
   return (
     <div className="flex flex-col gap-[32px] row-start-2 items-center ">
       <div>
         <h1 className="flex items-center py-8">My QR Code</h1>
-        <QrCodeGenerator qrData={qrCodeData} />
+        <QrCodeGenerator qrData={lineToken} />
       </div>
     </div>
   );
